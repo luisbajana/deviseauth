b7fec3db54eefd2aa7514319ce43035f072a19a9
Luis Bajana <lbajana@gmail.com>
2013-10-14 17:06:33 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Deviseauth::Application.config.secret_key_base = '6fecea8770abd0ba0f53a1a20a4fc555f57aa5ebaa742711f0370ceab9ba2bb63860b22344ac7f4676ca3d0700b20bd5c4348660eeae4c09ac71f5f11b26d9c6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Deviseauth::Application.config.session_store :cookie_store, key: '_deviseauth_session'
